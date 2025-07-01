b4a3621c6ab96599121ade6269cb19b390f0a95f
Amin Behnami <abehnami@berkeley.edu>
2025-07-01 02:12:16 -0700

    Fix: Restore original template and complete TODOs for Exercise 2

--- lab01/ex2_pointers_and_functions.c
+++ lab01/ex2_pointers_and_functions.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
 
-void add_one(int *input);
-void add_one_pointer(int *input);
-void add_one_double_ptr(int **input);
+void add_one(int input) {
+  input += 1;
+}
+
+// TODO: create a pointer to input
+void add_one_pointer(int *input) {
+  // TODO: add one to the integer that input points to
+  *input += 1;
+}
+
+// TODO: create a pointer to a pointer to input
+void add_one_double_ptr(int **input) {
+  // TODO: add one to the integer that input doubly points to
+  **input += 1;
+}
 
 int main() {
-    int x = 5;
+  // Assign x (an integer) to 5
+  int x = 5;
 
-    add_one(&x);
-    printf("add_one: %d\n", x);
+  // Call add_one on x
+  add_one(x);
 
-    add_one_pointer(&x);
-    printf("add_one_pointer: %d\n", x);
+  // This line should print 5
+  // Why doesn't this work?
+  printf("add_one: %d\n", x);
 
-    int *y = &x;
-    add_one_double_ptr(&y);
-    printf("add_one_double_ptr: %d\n", x);
+  // Let's try using add_one_pointer
 
-    return 0;
+  // TODO: use add_one_pointer to increment x
+  // Hint: compare the type of x with the type of the argument
+  //       for add_one_pointer
+  add_one_pointer(&x);
+
+  // This line should print 6
+  printf("add_one_pointer: %d\n", x);
+
+  // TODO: Let's save the pointer to x in y
+  int *y = &x;
+
+  // TODO: use add_one_double_ptr to increment x again, using the pointer 
+
+  add_one_double_ptr(&y);
+
+  // This line should print 7
+  printf("add_one_double_ptr: %d\n", x);
+
+  return 0;
 }
 
-void add_one(int *input) {
-    *input += 1;
-}
-
-void add_one_pointer(int *input) {
-    *input += 1;
-}
-
-void add_one_double_ptr(int **input) {
-    **input += 1;
-}
-
-// Force refresh
